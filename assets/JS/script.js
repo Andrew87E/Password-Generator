042683c742906c8c26d8992733044a0e2959199c
Andrew87E <106359255+Andrew87E@users.noreply.github.com>
2022-06-22 15:51:45 -0500

    changed from checkboxes to user prompts

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,28 +1,24 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-let Allowed = {
-  Uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
-  Lowers: "qwertyuiopasdfghjklzxcvbnm",
-  Numbers: "1234567890",
-  Symbols: "!@#$%^&*"
-}
+const numUppers = Number(window.prompt("How many uppercase letters?", "2"));
+const numLowers = Number(window.prompt("How many lowercase letters?", "6"));
+const numNumbers = Number(window.prompt("How many numbers?", "2"));
+const numSymbols = Number(window.prompt("How many symbols would you like to use?", "2"));
+const numLength = Number(window.prompt("How long do you want your password to be?", '8-128'))
 
-var Allow = confirm("")
+var uppers = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+var lowers = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var symbols = ["!","@","#","$","%","^","&","*"];
 
-function userSelection() {
-  
-}
+var upperAnswer = uppers[Math.floor(Math.random() * numUppers.Number)]
+var lowerAnswer = lowers[Math.floor(Math.random() * numLowers.Number)]
+var numbersAnswer = numbers[Math.floor(Math.random() * numNumbers.Number)]
+var symbolsAnswer = symbols[Math.floor(Math.random() * numSymbols.Number)]
+var lengthAnswer = length
 
-if(Allowed.Uppers = true){
-
-}
-
-if(Allowed.Numbers = true){
-
-}
-
-if(Allowed.Symbols = true){
+function generatePassword() {
 
 }
 
@@ -30,17 +26,17 @@ if(Allowed.Symbols = true){
 
 
 
-const getRandomCharFromString = (str) => str.charAt(Math.floor(Math.random() * str.length))
-const generatePassword = (length = 16) => { // password will be @Param-length, default to 8, and have at least one upper, one lower, one number and one symbol
-  let pwd = "";
-  pwd += getRandomCharFromString(Allowed.Uppers); //pwd will have at least one upper
-  pwd += getRandomCharFromString(Allowed.Lowers); //pwd will have at least one lower
-  pwd += getRandomCharFromString(Allowed.Numbers); //pwd will have at least one number
-  pwd += getRandomCharFromString(Allowed.Symbols);//pwd will have at least one symbolo
-  for (let i = pwd.length; i < length; i++)
-      pwd += getRandomCharFromString(Object.values(Allowed).join('')); //fill the rest of the pwd with random characters
-  return pwd
-}
+// const getRandomCharFromString = (str) => str.charAt(Math.floor(Math.random() * str.length))
+// const generatePassword = (length = 16) => { // password will be @Param-length, default to 8, and have at least one upper, one lower, one number and one symbol
+//   let pwd = "";
+//   pwd += getRandomCharFromString(Allowed.Uppers); //pwd will have at least one upper
+//   pwd += getRandomCharFromString(Allowed.Lowers); //pwd will have at least one lower
+//   pwd += getRandomCharFromString(Allowed.Numbers); //pwd will have at least one number
+//   pwd += getRandomCharFromString(Allowed.Symbols);//pwd will have at least one symbolo
+//   for (let i = pwd.length; i < length; i++)
+//       pwd += getRandomCharFromString(Object.values(Allowed).join('')); //fill the rest of the pwd with random characters
+//   return pwd
+// }
 
 
 // Write password to the #password input
